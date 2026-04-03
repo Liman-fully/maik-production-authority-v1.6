23f14a9218c13992aa715ec2d8e84988c9b440da
Liman <liman@example.com>
2026-04-03 17:20:14 +0100

    chore: 清理冗余文件，移至历史记忆

--- backend-backup/src/modules/metadata/metadata.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TaxonomyService } from './taxonomy.service';
-import { KeywordPoolService } from './keyword-pool.service';
-
-@Module({
-  providers: [TaxonomyService, KeywordPoolService],
-  exports: [TaxonomyService, KeywordPoolService],
-})
-export class MetadataModule {}
